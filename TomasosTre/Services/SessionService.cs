66935b520948b38b564bab8d910b9be076ace7c3
Johannes Smidelöv <johannes@smidelov.com>
2017-08-28 21:30:02 +0200

    Added Save overload for OrderRowIngredient

--- TomasosTre/Services/SessionService.cs
+++ TomasosTre/Services/SessionService.cs
@@ -21,6 +21,7 @@ namespace TomasosTre.Services
         /// </summary>
         /// <returns>Current Order</returns>
         public static List<OrderRow> Load(HttpContext HttpContext)
+            // TODO Find clever way to load from different types. Add out variable and accept consequences?
         {
             List<OrderRow> order;
             if (HttpContext.Session.GetString("Order") == null)
@@ -34,6 +35,16 @@ namespace TomasosTre.Services
             }
 
             return order;
+        }
+
+        /// <summary>
+        /// Saves current Order to session variable
+        /// </summary>
+        /// <param name="ori">Current Order Row Ingredients</param>
+        public static void Save(HttpContext HttpContext, List<OrderRowIngredient> ori)
+        {
+            var serializedValue = JsonConvert.SerializeObject(ori);
+            HttpContext.Session.SetString("ORI", serializedValue);
         }
     }
 }
