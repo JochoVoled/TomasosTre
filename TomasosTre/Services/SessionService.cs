bf2577154e1536fd8392ec738b714afb1c934ac3
JochoVoled <johannes@smidelov.com>
2017-08-28 14:56:21 +0200

    Added first draft on a service. No attempts made to make it work properly

---
+++ TomasosTre/Services/SessionService.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using TomasosTre.Models;
+//using static Microsoft.AspNetCore.Http.HttpContext;
+
+namespace TomasosTre.Services
+{
+    // This is a draft service, written intended to refactor useful methods from controllers
+    // However, there are strange issues with HttpContext, so I'll wait fixing those until later
+    public class SessionService
+    {
+        public HttpContext HttpContext { get; }
+
+
+        /// <summary>
+        /// Saves current Order to session variable
+        /// </summary>
+        /// <param name="order">Current Order</param>
+        private void Save(List<OrderRow> order)
+        {
+            var serializedValue = JsonConvert.SerializeObject(order);
+            HttpContext.Session.SetString("Order", serializedValue);
+        }
+        /// <summary>
+        /// Loads current Order from session variable
+        /// </summary>
+        /// <returns>Current Order</returns>
+        private List<OrderRow> Load()
+        {
+            List<OrderRow> order;
+            if (HttpContext.Session.GetString("Order") == null)
+            {
+                order = new List<OrderRow>();
+            }
+            else
+            {
+                var str = HttpContext.Session.GetString("Order");
+                order = JsonConvert.DeserializeObject<List<OrderRow>>(str);
+            }
+
+            return order;
+        }
+    }
+}
