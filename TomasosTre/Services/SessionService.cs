f5e09ecd2ee7d50353b297fa72d45a041c69b353
JochoVoled <johannes@smidelov.com>
2017-08-30 10:10:47 +0200

    Added Ignore setting to ReferenceLoopHandling, solving Exception upon customizing dish

--- TomasosTre/Services/SessionService.cs
+++ TomasosTre/Services/SessionService.cs
@@ -16,7 +16,7 @@ namespace TomasosTre.Services
         /// <param name="order">Current Order</param>
         public static void Save(HttpContext httpContext,List<OrderRow> order)
         {
-            var serializedValue = JsonConvert.SerializeObject(order);
+            var serializedValue = JsonConvert.SerializeObject(order, new JsonSerializerSettings { ReferenceLoopHandling = ReferenceLoopHandling.Ignore });
             httpContext.Session.SetString("Order", serializedValue);
         }
 
@@ -27,36 +27,7 @@ namespace TomasosTre.Services
         /// <param name="ori">Current Order Row Ingredients</param>
         public static void Save(HttpContext httpContext, List<OrderRowIngredient> ori)
         {
-            /*TODO Fix self referencing loop
-              Newtonsoft.Json.JsonSerializationException occurred
-              HResult=0x80131500
-              Message=Self referencing loop detected for property 'Dish' with type 'TomasosTre.Models.Dish'. Path '[0].OrderRow.Dish.DishIngredients[0]'.
-              Source=<Cannot evaluate the exception source>
-              StackTrace:
-               at Newtonsoft.Json.Serialization.JsonSerializerInternalWriter.CheckForCircularReference(JsonWriter writer, Object value, JsonProperty property, JsonContract contract, JsonContainerContract containerContract, JsonProperty containerProperty)
-               at Newtonsoft.Json.Serialization.JsonSerializerInternalWriter.CalculatePropertyValues(JsonWriter writer, Object value, JsonContainerContract contract, JsonProperty member, JsonProperty property, JsonContract& memberContract, Object& memberValue)
-               at Newtonsoft.Json.Serialization.JsonSerializerInternalWriter.SerializeObject(JsonWriter writer, Object value, JsonObjectContract contract, JsonProperty member, JsonContainerContract collectionContract, JsonProperty containerProperty)
-               at Newtonsoft.Json.Serialization.JsonSerializerInternalWriter.SerializeValue(JsonWriter writer, Object value, JsonContract valueContract, JsonProperty member, JsonContainerContract containerContract, JsonProperty containerProperty)
-               at Newtonsoft.Json.Serialization.JsonSerializerInternalWriter.SerializeList(JsonWriter writer, IEnumerable values, JsonArrayContract contract, JsonProperty member, JsonContainerContract collectionContract, JsonProperty containerProperty)
-               at Newtonsoft.Json.Serialization.JsonSerializerInternalWriter.SerializeValue(JsonWriter writer, Object value, JsonContract valueContract, JsonProperty member, JsonContainerContract containerContract, JsonProperty containerProperty)
-               at Newtonsoft.Json.Serialization.JsonSerializerInternalWriter.SerializeObject(JsonWriter writer, Object value, JsonObjectContract contract, JsonProperty member, JsonContainerContract collectionContract, JsonProperty containerProperty)
-               at Newtonsoft.Json.Serialization.JsonSerializerInternalWriter.SerializeValue(JsonWriter writer, Object value, JsonContract valueContract, JsonProperty member, JsonContainerContract containerContract, JsonProperty containerProperty)
-               at Newtonsoft.Json.Serialization.JsonSerializerInternalWriter.SerializeObject(JsonWriter writer, Object value, JsonObjectContract contract, JsonProperty member, JsonContainerContract collectionContract, JsonProperty containerProperty)
-               at Newtonsoft.Json.Serialization.JsonSerializerInternalWriter.SerializeValue(JsonWriter writer, Object value, JsonContract valueContract, JsonProperty member, JsonContainerContract containerContract, JsonProperty containerProperty)
-               at Newtonsoft.Json.Serialization.JsonSerializerInternalWriter.SerializeObject(JsonWriter writer, Object value, JsonObjectContract contract, JsonProperty member, JsonContainerContract collectionContract, JsonProperty containerProperty)
-               at Newtonsoft.Json.Serialization.JsonSerializerInternalWriter.SerializeValue(JsonWriter writer, Object value, JsonContract valueContract, JsonProperty member, JsonContainerContract containerContract, JsonProperty containerProperty)
-               at Newtonsoft.Json.Serialization.JsonSerializerInternalWriter.SerializeList(JsonWriter writer, IEnumerable values, JsonArrayContract contract, JsonProperty member, JsonContainerContract collectionContract, JsonProperty containerProperty)
-               at Newtonsoft.Json.Serialization.JsonSerializerInternalWriter.SerializeValue(JsonWriter writer, Object value, JsonContract valueContract, JsonProperty member, JsonContainerContract containerContract, JsonProperty containerProperty)
-               at Newtonsoft.Json.Serialization.JsonSerializerInternalWriter.Serialize(JsonWriter jsonWriter, Object value, Type objectType)
-               at Newtonsoft.Json.JsonSerializer.SerializeInternal(JsonWriter jsonWriter, Object value, Type objectType)
-               at Newtonsoft.Json.JsonConvert.SerializeObjectInternal(Object value, Type type, JsonSerializer jsonSerializer)
-               at Newtonsoft.Json.JsonConvert.SerializeObject(Object value)
-               at TomasosTre.Services.SessionService.Save(HttpContext HttpContext, List`1 ori) in C:\Source\Repos\TomasosTre\TomasosTre\Services\SessionService.cs:line 26
-               at TomasosTre.Controllers.ApiController.CustomizedDish(Int32 baseDishId, List`1 isOrderedIngredients) in C:\Source\Repos\TomasosTre\TomasosTre\Controllers\ApiController.cs:line 158
-               at Microsoft.Extensions.Internal.ObjectMethodExecutor.Execute(Object target, Object[] parameters)
-               at Microsoft.AspNetCore.Mvc.Internal.ControllerActionInvoker.<InvokeActionMethodAsync>d__12.MoveNext()
-            */
-            var serializedValue = JsonConvert.SerializeObject(ori);
+            var serializedValue = JsonConvert.SerializeObject(ori, new JsonSerializerSettings{ReferenceLoopHandling = ReferenceLoopHandling.Ignore});
             httpContext.Session.SetString("ORI", serializedValue);
         }
         #endregion
