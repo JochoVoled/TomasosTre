ffb656454b0d3a85d446952684e8a9c95af5c5c6
JochoVoled <johannes@smidelov.com>
2017-09-15 16:19:39 +0200

    Added environment-dependent connection strings

---
+++ TomasosTre/Data/ApplicationDbContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TomasosTre.Data
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            //optionsBuilder.UseSqlite("Data Source=blog.db");
+            var connectionString = "Server=.;Database=TomasosTre;Trusted_Connection=True;MultipleActiveResultSets=true";
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ TomasosTre/Data/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace TomasosTre.Data
+{
+    public class Constants
+    {
+        public const string DEVELOPMENT_ENVIRONMENT = "Development";
+        public const string STAGED_ENVIRONMENT = "Staged";
+        public const string PRODUCTION_ENVIRONMENT = "Production";
+    }
+}
